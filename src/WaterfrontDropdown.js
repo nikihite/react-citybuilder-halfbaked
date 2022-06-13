e24a218d3cd621899dffbac60be6de94902b7d04
Niki Hite <nikihite@comcast.net>
2022-06-13 12:17:03 -0700

    WaterfrontDropdown

--- src/WaterfrontDropdown.js
+++ src/WaterfrontDropdown.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
-export default function WaterfrontDropdown(props) {
+export default function WaterfrontDropdown({ setWaterfront }) {
   return (
     <div>
       Select a Waterfront
-      {/* this component should have a prop called setWaterfront, which is a function */}
-      {/* on change, this component should use e.target.value to pass the correct id to the setWaterfront function */}
-      {/* note that the options will need value properties (1, 2, or 3) that correspond to the photo filenames in the public directory */}
-      <select>
-        <option>Dock</option>
-        <option>Sunset</option>
-        <option>Boats</option>
+      <select onChange={e => setWaterfront(e.target.value)}>
+        <option value="dock">Dock</option>
+        <option value="sunset">Sunset</option>
+        <option value="boats">Boats</option>
       </select>
     </div>
   );
 }
+
+{/* this component should have a prop called setWaterfront, which is a function */}
+{/* on change, this component should use e.target.value to pass the correct id to the setWaterfront function */}
+{/* note that the options will need value properties (1, 2, or 3) that correspond to the photo filenames in the public directory */}
